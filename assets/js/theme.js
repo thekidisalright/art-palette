10042a27f923d4d2d8cf2ed23a8e6b1b92074472
thekidisalright <dev.alexandresilva@gmail.com>
2023-10-30 15:19:23 -0300

    php deleted and static made

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -6,4 +6,11 @@ themeButton.onclick = function () {
     theme = theme === 'light' ? 'dark' : 'light';
     themeStyle.href = 'assets/css/' + theme + '.css';
     localStorage.setItem('theme', theme);
+    window.location.reload();
 };
+
+if (theme === 'dark') {
+    themeButton.innerHTML = '<i class="fas fa-sun"></i>';
+} else {
+    themeButton.innerHTML = '<i class="fas fa-moon"></i>';
+}
