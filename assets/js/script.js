f0468cb902622b0f2766da1da116de6cf38330c6
Alexandre Silva <dev.alexandresilva@gmail.com>
2023-11-07 14:14:16 +0000

    Update CSS and JS files, add new modal button

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,31 +25,8 @@ function copiarHex(element){
     });
 }
 
-document.addEventListener("mousemove", (e) => {
-    const cursor = document.querySelector(".custom-cursor");
-    cursor.style.top = e.clientY + "px";
-    cursor.style.left = e.clientX + "px";
-});
 
-const elements = document.querySelectorAll("*");
-elements.forEach(element => {
-    element.addEventListener("mouseover", (e) => {
-        const cursor = document.querySelector("#cursor");
-        if (e.target.tagName === "A" || e.target.tagName === "BUTTON" || e.target.tagName === "INPUT") {
-            cursor.setAttribute("src", "assets/images/roller.svg");
-        } else {
-            cursor.setAttribute("src", "assets/images/brush.svg");
-        }
-    });
-});
 
-function toggleCursor() {
-    if (window.innerWidth < 768) {
-        document.querySelector(".custom-cursor").style.display = "none";
-    } else {
-        document.querySelector(".custom-cursor").style.display = "block";
-    }
-}
 
 // Select all phone and desktop buttons
 const buttons = document.querySelectorAll('.celular, .desktop');
@@ -102,11 +79,4 @@ desktops.forEach(desktop => {
   desktop.setAttribute('data-bs-toggle', 'modal');
   desktop.setAttribute('data-bs-target', '#modalDesktop');
 });
-
-
-
-
-toggleCursor();
-
-window.addEventListener("resize", toggleCursor);
 
